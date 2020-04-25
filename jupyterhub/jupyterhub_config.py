462c7987c2bfb497741d683c0168a2ac6ee8b5f5
mike38 <mike@alezan.org>
2020-04-25 17:12:04 +0000

    Initial commit

---
+++ jupyterhub/jupyterhub_config.py
@@ -0,0 +1,78 @@
+# JupyterHub configuration
+#
+## If you update this file, do not forget to delete the `jupyterhub_data` volume before restarting the jupyterhub service:
+##
+##     docker volume rm jupyterhub_jupyterhub_data
+##
+## or, if you changed the COMPOSE_PROJECT_NAME to <name>:
+##
+##    docker volume rm <name>_jupyterhub_data
+##
+
+import os
+# Set the log level by value or name.
+c.JupyterHub.log_level = 'DEBUG'
+
+# Enable debug-logging of the single-user server
+#c.Spawner.debug = True
+
+# Enable debug-logging of the single-user server
+#c.LocalProcessSpawner.debug = True
+
+## Generic
+c.JupyterHub.admin_access = True
+#c.Spawner.default_url = '/lab'
+
+## Authenticator
+#c.LocalAuthenticator.create_system_users = True
+#c.LocalAuthenticator.add_user_cmd = ['useradd', '-m'] 
+#c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+#c.DummyAuthenticator.password = "ChampoForEver"
+#from jhub_cas_authenticator.cas_auth import CASAuthenticator
+#c.JupyterHub.authenticator_class = CASAuthenticator
+#c.JupyterHub.bind_url = 'http://127.0.0.1:800/'
+c.JupyterHub.base_url = '/test'
+# The CAS URLs to redirect (un)authenticated users to.
+#c.CASAuthenticator.cas_login_url = 'https://cas.uvsq.fr/login'
+#c.CASLocalAuthenticator.cas_logout_url = 'https://cas.uvsq/logout'
+
+# The CAS endpoint for validating service tickets.
+#c.CASAuthenticator.cas_service_validate_url = 'https://cas.uvsq.fr/serviceValidate'
+
+# The service URL the CAS server will redirect the browser back to on successful authentication.
+#c.CASAuthenticator.cas_service_url = 'https://%s/hub/login' % os.environ['HOST']
+
+c.Authenticator.admin_users = { 'mike' }
+
+
+## Docker spawner
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+# user data persistence
+# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+
+# Other stuff
+c.Spawner.cpu_limit = 1
+c.Spawner.mem_limit = '10G'
+
+c.JupyterHub.authenticator_class = 'ltiauthenticator.LTIAuthenticator'
+
+c.LTIAuthenticator.consumers = {
+"bc31beb41e9065d459b33b2c9430a5b6fbdcfe310d29e4e4c9789e275fa7702b": "b5fb2b27fe34b61c95727b8f8395fe144d8c99356133eaa550af90b87b573577"
+                     }
+
+## Services
+#c.JupyterHub.services = [
+#    {
+#        'name': 'cull_idle',
+#        'admin': True,
+#        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
+#    },
+#]
